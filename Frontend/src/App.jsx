6b736303839c5e0bb32ff094d2f0211a5162445b
Utkarsh-Singh-u <utkarshsinghgkp2004@gmail.com>
2025-03-08 13:50:59 +0530

    Finally

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="text-3xl font-bold underline">BEST OF LUCK GUYS</div>;
+  return <div className="text-3xl font-bold underline">BEST OF LUCK GUYS, Hello everyone</div>;
 }
 
 export default App;
